a815211561127ab2a9e0a69bcfbd999ee4a300df
arunbabusubramaniam@gmail.com <arunbabusubramaniam@gmail.com>
2019-06-29 10:33:36 -0600

    Added problem Description

--- MaxBinaryTreeDepth.java
+++ MaxBinaryTreeDepth.java
@@ -1,11 +1,15 @@
-package com.arun.tree.binary;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/*Given a binary tree, find its maximum depth.
+ * The maximum depth is the number of nodes 
+ * along the longest path from the root node down to the farthest leaf node.
+ * Note: A leaf is a node with no children.
+ * Example:Given binary tree [3,9,20,null,null,15,7]
+ */
 public class MaxBinaryTreeDepth
 {
 
