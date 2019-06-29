4b3e3e925b8f332419233f2c882b92ad8dcfe461
arunbabusubramaniam@gmail.com <arunbabusubramaniam@gmail.com>
2019-06-28 21:08:51 -0600

    Reverse String and Reverse vowels in the string

---
+++ ReverseVowelsInString.java
@@ -0,0 +1,52 @@
+package com.arun.string;
+
+/**
+ * @author ASubramaniam
+ * 
+ * Write a function that takes a string as input and reverse only the vowels of a string.
+ */
+public class ReverseVowelsInString {
+
+	public static void main(String[] args) {
+		String input = "leetcode";
+		String outArr = reverse(input);
+		System.out.println(outArr);
+	}
+
+	private static String reverse(String arr) {
+		char[] inArr = arr.toCharArray();
+		int start = 0;
+		int end = inArr.length - 1;
+		String vowel = "AEIOUaeiou";
+
+		while ( start < end ) {
+			boolean startFlag = false;
+			boolean endFlag = false;
+			if( vowel.indexOf( inArr[ start ] ) != -1 ) 
+			{
+				startFlag = true;
+			} 
+			else
+			{
+				start ++;
+			}
+			if( vowel.indexOf( inArr[ end ] ) != -1 )
+			{
+				endFlag = true;
+			} 
+			else
+			{
+				end --;
+			}
+			if ( startFlag && endFlag)
+			{
+				char temp = inArr[ start ];
+				inArr[ start ] = inArr[ end ];
+				inArr[ end ] =  temp;
+				start ++;
+				end --;
+			}
+		}
+		return new String ( inArr );
+	}
+}
